633937e19a817c7fe203523eb9511c3d5d610ebe
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-09-17 15:19:55 +0530

    "feature- added React Challenges"

---
+++ react/React Challenges/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElem = ReactDOM.createRoot(document.getElementById('root'));
+rootElem.render(
+ <React.StrictMode>
+  <App />
+ </React.StrictMode>
+)
